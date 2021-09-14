692a66b750d67aabeb4c7ff7c549982e6001f0a9
Lindsay Lopez <lopez95@berkeley.edu>
2021-09-14 13:06:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import './App.css';
-import ClickCounter2 from './components/ClickCounter2';
-import HoverCounter2 from './components/HoverCounter2';
-import User from './components/User';
-import Counter from './components/Counter';
-
 function App() {
   return (
     <div className='App'>
-      <Counter
-        render={(count, incrementCount) => (
-          <ClickCounter2 count={count} incrementCount={incrementCount} />
-        )}
-      />
-      <Counter
-        render={(count, incrementCount) => (
-          <HoverCounter2 count={count} incrementCount={incrementCount} />
-        )}
-      />
-      {/* <ClickCounter2 />
-      <HoverCounter2 />
-      <User render={(isLoggedIn) => isLoggedIn ? 'Linda' : 'Guest'} /> */}
+      
     </div>
   );
 }
